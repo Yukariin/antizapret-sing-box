7ec62f6eb14666b87df4efb4944b20cb9389affc
Yukari <mistelinn@gmail.com>
2024-08-24 11:16:02 +0300

    Update main.go

--- cmd/githubreleaser/main.go
+++ cmd/githubreleaser/main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/Yukariin/antizapret-sing-box/geosite_antizapret"
 	"log"
+	"os"
+
+	"github.com/Yukariin/antizapret-sing-box/geosite_antizapret"
 )
 
 func main() {
@@ -20,12 +22,13 @@ func main() {
 
 	if token == "" || owner == "" || repo == "" {
 		fmt.Println("You should provide token, owner and repo name!")
-		return
+		flag.Usage()
+		os.Exit(1)
 	}
 
 	generator := geosite_antizapret.NewGenerator(geosite_antizapret.WithGitHubClient(token, owner, repo))
 
 	if err := generator.GenerateAndUpload(context.Background()); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to generate and upload assets: %v", err)
 	}
 }
